31cbbe7aebd562b985cc3a4794b463928d6ef3da
Plamen Simeonov <haibusa1991@gmail.com>
2023-03-10 20:32:04 +0200

    home controller tuning

--- server/src/main/java/com/snews/server/controllers/HomeController.java
+++ server/src/main/java/com/snews/server/controllers/HomeController.java
@@ -3,15 +3,28 @@ package com.snews.server.controllers;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomeController implements ErrorController {
 
 
-    @GetMapping("/")
-    public String getHome(){
-        return "index.html";
+//    @GetMapping("/")
+//    public String getHome(){
+//        return "index.html";
+//    }
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value ="/error")
+    public String error() {
+        return "forward:/index.html";
+    }
+
+    //    @Override
+    public String getErrorPath() {
+        return PATH;
     }
 }
 
